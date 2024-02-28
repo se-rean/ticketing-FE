dde3f1e76c0327ad6f19d5fbf83ecdc0bd8701b4
Rbandoy <reneboy.andoy@e-horizon.com>
2024-02-29 00:09:19 +0800

    add defalt timezone in offset

--- src/utils/date-formats.js
+++ src/utils/date-formats.js
@@ -1,11 +1,11 @@
 import moment from 'moment';
 
-export const dateTime = (datetime, offset = null) => {
-  if (offset) {
-    return moment(datetime).utcOffset(offset).format('MMMM DD, YYYY h:mm A');
-  } else {
-    return moment(datetime).format('MMMM DD, YYYY h:mm A');
-  }
+export const dateTime = (datetime, offset = 4) => {
+  // if (offset) {
+  // return moment(datetime).utcOffset(offset).format('DD, MMMM, YYYY h:mm A');
+  // } else {
+  return moment(datetime).utcOffset(offset).format('DD MMMM, YYYY h:mm A');
+  // }
 
 };
 
